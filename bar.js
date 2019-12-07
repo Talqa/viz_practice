4e9222e4c845633ae166973fd8152a7038eb073a
Alzbeta Talarovicova <7968267+Talqa@users.noreply.github.com>
2019-12-07 03:16:30 -0500

    add scales, formatting

--- bar.js
+++ bar.js
@@ -1,112 +1,127 @@
-    //variables:
-    var w = 600;
-    var h = 400;
-    var barPadding = 1;
+// variables:
+var w = 600
+var h = 400
+var barPadding = 1
+var padding = 15
 
-    var svg = d3.select("body")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h);
+var svg = d3.select('body')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h)
 
-    //helper functions:
-    var rowConverter = function(d) {
-        //convert data from string to other formats where needed
-        //below are columns of the full dataset
-        // return {
-        //     Critic_Score: parseFloat(d.Critic_Score),
-        //     Developer: d.Developer,
-        //     ESRB_Rating: d.ESRB_Rating,
-        //     Genre: d.Genre,
-        //     Global_Sales: parseFloat(d.Global_Sales),
-        //     JP_Sales: parseFloat(d.JP_Sales),
-        //     Last_Update: d.Last_Update,
-        //     NA_Sales: parseFloat(d.NA_Sales),
-        //     Name: d.Name,
-        //     Other_Sales: parseFloat(d.Other_Sales),
-        //     PAL_Sales: parseFloat(d.PAL_Sales),
-        //     Platform: d.Platform,
-        //     Publisher: d.Publisher,
-        //     Rank: parseFloat(d.Rank),
-        //     Total_Shipped: parseFloat(d.Total_Shipped),
-        //     User_Score: parseFloat(d.User_Score),
-        //     VGChartz_Score: parseFloat(d.VGChartz_Score),
-        //     Vgchartzscore: parseFloat(d.Vgchartzscore),
-        //     Year: parseFloat(d.Year),
-        //     basename: d.basename,
-        //     img_url: d.img_url,
-        //     status: d.status,
-        //     url: d.url
-        // };
-        return { //num games per publisher file
-            Publisher: d.Publisher,
-            Year: +d.Year,
-            Num_games: +d.Num_games
-        };
-    }
-    
-    //dataset
-    var dataset;
+var parseYear = d3.timeParse('%Y')
 
-    //load and use dataset
-    //everything you want to do needs to be within the data loading function (csv)
-    //d3.csv("data/vgsales-12-4-2019.csv", rowConverter, function(data) {
-    d3.csv("data/vgsales-12-4-2019_num_publishers.csv", rowConverter, function(data) {
-        dataset = data;
-        
-        //console.table(data);
-        //console.log(data);
-    
-        // partial data selection for testing
-        newdata = dataset.filter(function(d) {return d.Publisher=="Nintendo"; });
-        
-        
-        //draw bars
-            svg.selectAll("rect")
-                .data(newdata)
-                .enter()
-                .append("rect")
-                .attr("x", function(d,i) {
-                    return i * (w / newdata.length);
-                })
-                .attr("y", function(d) {
-                    return h - d.Num_games * 2;
-                })
-                .attr("width", w / newdata.length - barPadding)
-                .attr("height", function(d) {
-                    return d.Num_games * 2;
-                })
-                .attr("fill", function(d) {
-                    return "rgb(0, 0, " + Math.round(d.Num_games * 5) + ")";
-                });
+// helper functions:
+var rowConverter = function (d) {
+  // convert data from string to other formats where needed
+  // below are columns of the full dataset
+  // return {
+  //     Critic_Score: parseFloat(d.Critic_Score),
+  //     Developer: d.Developer,
+  //     ESRB_Rating: d.ESRB_Rating,
+  //     Genre: d.Genre,
+  //     Global_Sales: parseFloat(d.Global_Sales),
+  //     JP_Sales: parseFloat(d.JP_Sales),
+  //     Last_Update: d.Last_Update,
+  //     NA_Sales: parseFloat(d.NA_Sales),
+  //     Name: d.Name,
+  //     Other_Sales: parseFloat(d.Other_Sales),
+  //     PAL_Sales: parseFloat(d.PAL_Sales),
+  //     Platform: d.Platform,
+  //     Publisher: d.Publisher,
+  //     Rank: parseFloat(d.Rank),
+  //     Total_Shipped: parseFloat(d.Total_Shipped),
+  //     User_Score: parseFloat(d.User_Score),
+  //     VGChartz_Score: parseFloat(d.VGChartz_Score),
+  //     Vgchartzscore: parseFloat(d.Vgchartzscore),
+  //     Year: parseFloat(d.Year),
+  //     basename: d.basename,
+  //     img_url: d.img_url,
+  //     status: d.status,
+  //     url: d.url
+  // };
+  return { // num games per publisher file
+    Publisher: d.Publisher,
+    Year: parseYear(d.Year), // +d.Year,
+    Num_games: +d.Num_games
+  }
+}
 
-        //add labels
-                svg.selectAll("text")
-                   .data(newdata)
-                   .enter()
-                   .append("text")
-                   .text(function(d) {
-                       return d.Num_games;
-                   })
-                   .attr("x", function(d,i) {
-                    return i * (w / newdata.length) + (w / newdata.length - barPadding) / 2;
-                    })
-                    .attr("y", function(d) {
-                        if (d.Num_games > 10) {
-                            return h - (d.Num_games * 2) + 10;
-                        } else {
-                            return h - (d.Num_games * 2) - 5;
-                        }
-                    })
-                    .attr("text-anchor", "middle")
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", "10px")
-                    .attr("fill", function(d) {
-                        if (d.Num_games > 10) {
-                            return "white";
-                        } else {
-                            return "black";
-                        }
-                    });
+// dataset
+var dataset
 
-    });
-          
+// load and use dataset
+// everything you want to do needs to be within the data loading function (csv)
+// d3.csv("data/vgsales-12-4-2019.csv", rowConverter, function(data) {
+d3.csv('data/vgsales-12-4-2019_num_publishers.csv', rowConverter, function (data) {
+  dataset = data
+
+  // console.table(data);
+  // console.log(data);
+
+  // partial data selection for testing
+  var newdata = dataset.filter(function (d) { return d.Publisher === 'Nintendo' })
+  console.log(newdata)
+
+  var xScale = d3.scaleTime()
+    .domain([
+      d3.min(newdata, function (d) { return d.Year }),
+      d3.max(newdata, function (d) { return d.Year })
+    ])
+    .range([padding, w - padding])
+
+  // var yScale = d3.scaleLinear()
+  //   .domain([d3.min(newdata, function (d) { return d.Num_games }),
+  //     d3.max(newdata, function (d) { return d.Num_games })])
+  //   .range([0, h])
+
+  // draw bars
+  svg.selectAll('rect')
+    .data(newdata)
+    .enter()
+    .append('rect')
+    .attr('x', function (d, i) {
+      // return i * (w / newdata.length)
+      return xScale(d.Year)
+    })
+    .attr('y', function (d) {
+      return h - d.Num_games * 2
+    })
+    .attr('width', w / newdata.length - barPadding)
+    .attr('height', function (d) {
+      return d.Num_games * 2
+    })
+    .attr('fill', function (d) {
+      return 'rgb(0, 0, ' + Math.round(d.Num_games * 5) + ')'
+    })
+
+  // add labels
+  svg.selectAll('text')
+    .data(newdata)
+    .enter()
+    .append('text')
+    .text(function (d) {
+      return d.Num_games
+    })
+    .attr('x', function (d, i) {
+      // return i * (w / newdata.length) + (w / newdata.length - barPadding) / 2
+      return xScale(d.Year) + 5
+    })
+    .attr('y', function (d) {
+      if (d.Num_games > 10) {
+        return h - (d.Num_games * 2) + 10
+      } else {
+        return h - (d.Num_games * 2) - 5
+      }
+    })
+    .attr('text-anchor', 'middle')
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', '10px')
+    .attr('fill', function (d) {
+      if (d.Num_games > 10) {
+        return 'white'
+      } else {
+        return 'black'
+      }
+    })
+})
--- scatter.js
+++ scatter.js
@@ -1,71 +1,95 @@
-//variables:
-var w = 600;
-var h = 400;
+// variables:
+var w = 600
+var h = 400
+var padding = 15
 
-var svg = d3.select("body")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
+var svg = d3.select('body')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h)
 
-//helper functions:
-var rowConverter = function(d) {
-    //convert data from string to other formats where needed
-    return {
-        Critic_Score: +d.Critic_Score,
-        Name: d.Name,
-        Publisher: d.Publisher,
-        Year: +d.Year,
-        NA_Sales: +d.NA_Sales
-    };
+// helper functions:
+var rowConverter = function (d) {
+  // convert data from string to other formats where needed
+  return {
+    Critic_Score: +d.Critic_Score,
+    Name: d.Name,
+    Publisher: d.Publisher,
+    Year: +d.Year,
+    NA_Sales: +d.NA_Sales
+  }
 }
 
-    //dataset
-    var dataset;
+// dataset
+var dataset
 
-    //load and use dataset
-    //everything you want to do needs to be within the data loading function (csv)
-    d3.csv("data/vgsales-12-4-2019_critic_score.csv", rowConverter, function(data) {
-        dataset = data;
-        
-        //console.table(data);
-        //console.log(data);
-    
-        // partial data selection for testing
-        newdata = dataset.filter(function(d) {
-            return d.Publisher=="Nintendo" 
-            });
+// load and use dataset
+// everything you want to do needs to be within the data loading function (csv)
+d3.csv('data/vgsales-12-4-2019_critic_score.csv', rowConverter, function (data) {
+  dataset = data
 
-        //add circles
-        svg.selectAll("circle")  
-           .data(newdata)
-           .enter()
-           .append("circle")  
-           .attr("cx", function(d) {
-               return d.Critic_Score * 50;
-           })
-           .attr("cy", function(d) {
-               return h - (d.NA_Sales * 100) - 10;
-           })
-           .attr("r", function(d) {
-               return Math.sqrt(d.NA_Sales * 100);
-           });
+  // console.table(data);
+  // console.log(data);
 
-           //add labels
-           svg.selectAll("text")  
-                .data(newdata)
-                .enter()
-                .append("text")
-                .text(function(d) {
-                    return d.Name;
-                })
-                .attr("x", function(d) {
-                    return d.Critic_Score * 50;
-                })
-                .attr("y", function(d) {
-                    return h - (d.NA_Sales * 100) - 10;
-                })
-                .attr("font-family", "sans-serif")
-                .attr("font-size", "11px")
-                .attr("fill", "red");
+  // partial data selection for testing
+  var newdata = dataset.filter(function (d) {
+    return d.Publisher === 'Nintendo'
+  })
 
-    });
+  var xScale = d3.scaleLinear()
+    .domain([d3.min(newdata, function (d) { return d.Critic_Score }),
+      d3.max(newdata, function (d) { return d.Critic_Score })])
+    .range([padding, w - padding])
+    .nice()
+
+  var yScale = d3.scaleLinear()
+    .domain([d3.min(newdata, function (d) { return d.NA_Sales }),
+      d3.max(newdata, function (d) { return d.NA_Sales })])
+    .range([h - padding, padding])
+    .nice()
+
+  var aScale = d3.scaleSqrt()
+    .domain([d3.min(newdata, function (d) { return d.NA_Sales }),
+      d3.max(newdata, function (d) { return d.NA_Sales })])
+    .range([2, 10])
+    .nice()
+
+  // add circles
+  svg.selectAll('circle')
+    .data(newdata)
+    .enter()
+    .append('circle')
+    .attr('cx', function (d) {
+    //   return d.Critic_Score * 50
+      return xScale(d.Critic_Score)
+    })
+    .attr('cy', function (d) {
+    //   return h - (d.NA_Sales * 100) - 10
+      return yScale(d.NA_Sales)
+    })
+    .attr('r', function (d) {
+    //   return Math.sqrt(d.NA_Sales * 100)
+      return aScale(d.NA_Sales)
+    })
+    .attr('fill', 'orange')
+
+  // add labels
+  svg.selectAll('text')
+    .data(newdata)
+    .enter()
+    .append('text')
+    .text(function (d) {
+      return d.NA_Sales
+    })
+    .attr('x', function (d) {
+    //   return d.Critic_Score * 50
+      return xScale(d.Critic_Score)
+    })
+    .attr('y', function (d) {
+    //   return h - (d.NA_Sales * 100) - 10
+      return yScale(d.NA_Sales)
+    })
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', '11px')
+    .attr('fill', 'blue')
+})
